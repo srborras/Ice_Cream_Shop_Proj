0cdd8d4ce106fe58a4c74385fa95caf7c50cf9eb
Sean Borras <srborras@qatar.cmu.edu>
2019-04-16 21:20:41 +0000

    Updated Flavors Model

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,2 +1,25 @@
 class Flavor < ApplicationRecord
+    
+    #Callbacks
+    #before_destroy :make_inactive
+    
+    #Relations
+    has_many :store_flavors
+    has_many :stores, through: :store_flavors
+    
+    #Validations
+    validates_presence_of :name
+    
+    #Scopes
+    scope :active, -> { where(active: true) }
+    scope :inactive, -> { where(active: false) }
+    scope :alphabetical, -> { order('name') }
+    
+    #Callback code
+    private
+    
+    def make_inactive
+        self.active = false
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_141008) do
+ActiveRecord::Schema.define(version: 2019_04_16_205553) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "store_id"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2019_04_16_141008) do
     t.date "date_of_birth"
     t.string "phone"
     t.string "role"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flavors", force: :cascade do |t|
+    t.string "name"
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
